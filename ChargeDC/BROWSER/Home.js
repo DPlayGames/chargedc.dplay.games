ce600cb52089cace8329a4762d7537c489c7d9fa
Hanul <hanul@hanul.me>
2019-10-12 03:03:03 +0900

    Uniswap 주소 변경

--- ChargeDC/BROWSER/Home.js
+++ ChargeDC/BROWSER/Home.js
@@ -120,7 +120,7 @@ ChargeDC.Home = CLASS({
 				c : MSG('UNISWAP_BUTTON'),
 				on : {
 					tap : () => {
-						open('https://uniswap.exchange');
+						open('https://uniswap.exchange/swap/0x92c5387aCE61F5c505BF2c2D4c84120F0A813d4B');
 					}
 				}
 			})]
